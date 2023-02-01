b7110ee6574352ef9e623e5544bff29824ca7d3c
P0PTTirby <123979677+P0PTTirby@users.noreply.github.com>
2023-02-01 14:54:21 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,125 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . # # . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # # . .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # . # #
+    . # # # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    # . . . #
+    . . . . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    # . # . #
+    . # # # .
+    # # # # #
+    . # # # .
+    # . # . #
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
-	
+    led.plotBrightness(2, 0, 270)
+    led.plotBrightness(2, 1, 270)
+    led.plotBrightness(2, 2, 270)
+    led.plotBrightness(1, 2, 70)
+    led.plotBrightness(3, 2, 70)
+    led.plotBrightness(1, 3, 70)
+    led.plotBrightness(3, 3, 70)
+    led.plotBrightness(2, 4, 70)
+    for (let index = 0; index < 1; index++) {
+        led.plotBrightness(1, 1, 270)
+        led.plotBrightness(3, 1, 270)
+    }
+    basic.pause(500)
+    for (let index = 0; index < 1; index++) {
+        led.plotBrightness(1, 1, 0)
+        led.plotBrightness(3, 1, 0)
+    }
+    basic.pause(500)
 })
